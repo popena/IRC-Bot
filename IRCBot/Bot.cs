5e07e2243459919e67c0d133677092c1310ac51b
elgis <aaro.a7@hotmail.com>
2014-12-05 18:50:53 +0200

    new files added

---
+++ IRCBot/Bot.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace IRCBot
+{
+    class Bot
+    {
+        private TcpClient irc;
+        private Stream data;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private string channel = "";
+
+        public Bot(string server, int port)
+        {
+            irc = new TcpClient(server, port);
+            data = irc.GetStream();
+            reader = new StreamReader(data);
+            writer = new StreamWriter(data) { NewLine = "\r\n", AutoFlush = true };
+        }
+
+        public void closeConnection()
+        {
+            irc.Close();
+            data.Close();
+            writer.Close();
+            reader.Close();
+        }
+
+        public string readMessage()
+        {
+            return reader.ReadLine().ToString();
+        }
+
+        public void sendMessage(string message)
+        {
+            writer.WriteLine("PRIVMSG " + channel + " :" + message);
+        }
+
+        public void connectToChannel(string channel, string nick)
+        {
+            this.channel = channel;
+            Console.WriteLine("Connecting, this may take a while...");
+
+            writer.WriteLine("USER irctestbotti completely real :testibotti");
+            writer.WriteLine("NICK " + nick);
+
+            Console.WriteLine("Waiting for ping request...");
+
+            string message = "";
+            while (true)
+            {
+                message = reader.ReadLine();
+                if (message.StartsWith("PING"))
+                {
+                    sendPONG(message);
+                    Console.WriteLine("Pong sent!");
+                    break;
+                }
+            }
+
+            writer.WriteLine("JOIN " + channel);
+
+            Console.WriteLine("Successfully connected!");
+            sendMessage("Elgis, my overlord, I'm answering to your summons");
+        }
+
+        public void sendPONG(string msg)
+        {
+            msg = msg.Split(' ')[1];
+            writer.WriteLine("PONG " + msg);
+        }
+
+        public void Functions(string msg)
+        {
+            if (msg.StartsWith("!greet"))
+                sendMessage("Hello " + msg.Split(' ')[1]);
+            else if (msg == "!date")
+                sendMessage(DateTime.Now.ToString());
+            else
+                Analyze(msg);
+        }
+
+        private void Analyze(string msg)
+        {
+            string linkPattern = @"((http(s)?://)?(www.)?(.*)?.+\.(.){1,})(.+)?";
+
+            if (Regex.IsMatch(msg, linkPattern, RegexOptions.Singleline))
+            {
+                string url = Regex.Match(msg, linkPattern, RegexOptions.Singleline).ToString();
+                if (url.StartsWith("//"))
+                    url = "http:" + url;
+                else if (!url.StartsWith("//"))
+                    url = "http://" + url;
+
+                string sourceCode = "";
+                string titleContent = "";
+
+                try
+                {
+                    WebClient wb = new WebClient();
+                    wb.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    Stream data = wb.OpenRead(url);
+                    StreamReader sr = new StreamReader(data);
+                    sourceCode = sr.ReadToEnd();
+
+                    string titlePattern = @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>";
+                    titleContent = Regex.Match(sourceCode, titlePattern, RegexOptions.Multiline).Groups["Title"].Value;
+
+                    titleContent = "[URL] " + WebUtility.HtmlDecode(titleContent);
+                }
+                catch (Exception ex)
+                {
+                    //error handling needs to be enhanced
+                }
+                sendMessage(titleContent);
+            }
+        }
+    }//class
+}//namespace
---
+++ IRCBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace IRCBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot ircbot = new Bot("chat.freenode.net", 6667);
+            ircbot.connectToChannel("#elgisbottest", "superPotti");
+
+            StreamWriter sr = new StreamWriter(@"../../../chatlog.txt");
+            string message = "";
+
+            while (true)
+            {
+                message = ircbot.readMessage();
+
+                if (message.StartsWith("PING"))
+                {
+                    ircbot.sendPONG(message);
+                    Console.WriteLine(message);
+                }
+
+                sr.WriteLine(formatMsg(message, 0));
+
+                if (formatMsg(message, 1) == "!quit")
+                    break;
+                else
+                    ircbot.Functions(formatMsg(message, 1));
+            }
+            sr.Close();
+            ircbot.closeConnection();
+        }
+
+        private static string formatMsg(string message, int flag)
+        {
+            string formatted = "";
+            string[] words = message.Split(':');
+
+            if (flag == 0)
+            {
+                var date = DateTime.Now;
+                formatted = "[" + date.Hour + ":" + date.Minute + "]<";
+                formatted += words[1] + "> ";//username,
+                formatted += words[words.Length - 1];
+            }
+            else
+            {
+                formatted = words[words.Length - 1];
+            }
+            return formatted;
+        }
+    }
+}
